0eebc7aaa23f0b9ad0e7c2a88fe9d70cb4572629
kaidranzerr <ashstark68@gmail.com>
2025-09-19 11:14:23 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='complete end to end project',
+    author='kaidranzerr',
+    license='MIT',
+)
