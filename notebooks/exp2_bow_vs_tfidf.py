03999c844b178e0b5ad3214771540842a1e02ccc
kaidranzerr <ashstark68@gmail.com>
2025-09-23 12:01:01 +0530

    logger added

--- notebooks/exp2_bow_vs_tfidf.py
+++ notebooks/exp2_bow_vs_tfidf.py
@@ -28,9 +28,9 @@ warnings.filterwarnings("ignore")
 CONFIG = {
     "data_path": "notebooks/data.csv",
     "test_size": 0.2,
-    "mlflow_tracking_uri": "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow",
-    "dagshub_repo_owner": "vikashdas770",
-    "dagshub_repo_name": "YT-Capstone-Project",
+    "mlflow_tracking_uri": "https://dagshub.com/kaidranzerr/MLOPS-capstone-project.mlflow",
+    "dagshub_repo_owner": "kaidranzerr",
+    "dagshub_repo_name": "MLOPS-capstone-project",
     "experiment_name": "Bow vs TfIdf"
 }
 
