f1e8c4051ffcbfab884e820c9b31c5e0f0939871
kaidranzerr <ashstark68@gmail.com>
2025-09-23 12:50:08 +0530

    changes in model evaluation

--- src/models/model_evaluation.py
+++ src/models/model_evaluation.py
@@ -22,8 +22,8 @@ os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
-repo_owner = "vikashdas770"
-repo_name = "YT-Capstone-Project"
+repo_owner = "kaidranzerr"
+repo_name = "MLOPS-capstone-project"
 
 # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
